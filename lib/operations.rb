6cf9c5aa0a7673c5fe9d583c6438d636adbb934d
Aiden Marques <aiden.marques94@gmail.com>
2020-09-12 14:50:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
-def unsafe?(speed)
+require "pry"
 
+def unsafe?(speed)
+  if speed == 40
+     "unsafe"
+  else
+      "safe"
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+  speed <40 || speed >60? "unsafe": "safe"
 end
 	
 
